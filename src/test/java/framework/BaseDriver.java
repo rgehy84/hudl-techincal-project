d302a23c1f54e77190149f40120c795bb3d835f9
ralphgehy <rgehy84@gmail.com>
2023-01-21 07:21:40 -0500

    added capabilities of configuring browser to run tests in BaseDriver file to set up driver and tear down driver after run

---
+++ src/test/java/framework/BaseDriver.java
@@ -0,0 +1,70 @@
+package framework;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.*;
+
+import java.time.Duration;
+
+public class BaseDriver {
+    WebDriver driver;
+
+    /***
+     * maximizes the browser window and sets in implicit wait of 15 seconds before timing out
+     * @return driver that can be used in tests without making the original driver accessible to the public
+     */
+    public WebDriver getDriver() {
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        return driver;
+    }
+
+
+    /***
+     * this uses the WebDriverManager to set the driver binaries. It avoids having to
+     * manually maintain the driver binaries. This will also download the latest version of the drivers
+     * @param browser => specifies the browser to prepare to testing
+     */
+    @Parameters("browser_to_run_tests")
+    @BeforeClass
+    static void setupClass(@Optional("CHROME") Browsers browser) {
+        switch (browser) {
+            case FIREFOX -> WebDriverManager.firefoxdriver().setup();
+            case CHROME -> WebDriverManager.chromedriver().setup();
+            case SAFARI -> WebDriverManager.safaridriver().setup();
+            case IE -> WebDriverManager.iedriver().setup();
+            case EDGE -> WebDriverManager.edgedriver().setup();
+        }
+
+    }
+
+    /***
+     * this method is used to create a new instance of the driver
+     * @param browser => specifies which browser to run the tests
+     */
+    @Parameters("browser_to_run_tests")
+    @BeforeMethod
+    public void testSetup(@Optional("CHROME") Browsers browser) {
+        switch (browser) {
+            case FIREFOX -> driver = new FirefoxDriver();
+            case CHROME -> driver = new ChromeDriver();
+            case SAFARI -> driver = new SafariDriver();
+            case IE -> driver = new InternetExplorerDriver();
+            case EDGE -> driver = new EdgeDriver();
+        }
+    }
+
+    /***
+     * this method is used to tear down the webdriver instance after each method is executed
+     */
+    @AfterMethod
+    public void teardownDriver() {
+        getDriver().close();
+    }
+
+}
---
+++ src/test/java/testcases/LoginTest.java
@@ -0,0 +1,14 @@
+package testcases;
+
+import framework.BaseDriver;
+import org.testng.annotations.*;
+public class LoginTest extends BaseDriver {
+
+
+
+    @Test
+    public void sampleFrameworkTest() {
+        getDriver().get("http://www.hudl.com/");
+    }
+
+}

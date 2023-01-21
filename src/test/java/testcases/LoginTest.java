fb8a0b68502c599aadfb2ae7f070df9aeb3b05fd
ralphgehy <rgehy84@gmail.com>
2023-01-21 08:20:43 -0500

    added testng xml runner file

--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -1,14 +1,22 @@
 package testcases;
 
 import framework.BaseDriver;
+import framework.WebDriverActions;
+import org.openqa.selenium.By;
 import org.testng.annotations.*;
 public class LoginTest extends BaseDriver {
+    private WebDriverActions wda;
 
-
+    @BeforeMethod
+    public void setupTests() {
+        wda = new WebDriverActions(getDriver());
+    }
 
     @Test
     public void sampleFrameworkTest() {
-        getDriver().get("http://www.hudl.com/");
+        wda.goToUrl("http://www.hudl.com");
+        wda.click(getDriver().findElement(By.cssSelector("div.mainnav__actions > a[data-qa-id='login']")), "Login button");
+        wda.enterTextIntoInputField(getDriver().findElement(By.id("email")), "Email", "rgehy84@gmail.com");
     }
 
 }
